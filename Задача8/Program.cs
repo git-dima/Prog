8d9d9c2d138430670cd3a1c20bd89d8005191069
git-dima <demonAVGUST@yandex.ru>
2022-12-20 04:56:31 +0300

    Задача 8

---
+++ Задача8/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5-> 2, 4
+// 8-> 2, 4, 6, 8
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число!");
+        string value = Console.ReadLine();
+
+        int a = int.Parse(value);
+        int b = 0;
+
+        if (a % 2 != 0)
+        {
+            a = a - 1;
+        }
+
+        do
+        {
+            b = b + 2;
+            Console.Write(b + ", ");
+        }
+        while (b < a - 2);
+        Console.Write(b + 2);
+    }
+}
+
+// Последние строчки сделаны для того,
+// чтобы избавиться от запятой в конце ответа, выводимого на экран.
